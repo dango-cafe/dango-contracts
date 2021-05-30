95d14019cddcdbf733f999ea744c29ea42780fe3
zetapsy <zetapsy@zohomail.com>
2021-05-30 17:07:56 +0530

    Add deployments

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+require("dotenv").config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const ALCHEMY_ID = process.env.ALCHEMY_ID;
+const INFURA_ID = process.env.INFURA_ID;
+const ETHERSCAN = process.env.ETHERSCAN;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,5 +18,25 @@ task("accounts", "Prints the list of accounts", async () => {
  */
 module.exports = {
   solidity: "0.7.6",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_ID}`,
+        blockNumber: 25155741,
+      },
+      blockGasLimit: 12000000,
+    },
+    kovan: {
+      url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN
+  }
 };
 
